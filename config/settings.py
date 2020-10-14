4d674824e7371e9f3960134f9da7e8e00ac45b5b
youngnrichhh <youngnrichhh@naver.com>
2020-10-14 16:00:30 +0900

    bootstrap template 적용

--- config/settings.py
+++ config/settings.py
@@ -128,6 +128,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'assets'),
+)
 
 LOGIN_REDIRECT_URL = '/'
