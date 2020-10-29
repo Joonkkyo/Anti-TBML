acc8bab1e943bba4112687c65d9ea1d9109d60cd
Joonkyo Seo <jkseo50@gmail.com>
2020-10-29 15:08:39 +0900

    search pagination 오류 수정

--- sanction/urls.py
+++ sanction/urls.py
@@ -4,8 +4,6 @@ from sanction import views
 app_name = 'sanction'
 
 urlpatterns = [
-    # path('', views.SanctionLV.as_view(), name='index'),
-    # path('', views.SanctionLV.as_view(), name='sanction_list'),
     path('', views.sanction_list, name='sanction_list'),
     path('search/', views.result, name='result'),
 ]
--- sanction/views.py
+++ sanction/views.py
@@ -2,20 +2,6 @@ from django.views.generic import ListView
 from .models import SanctionList
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
-from django.views.generic import CreateView, UpdateView, DeleteView
-
-
-# class SanctionLV(ListView):
-#     model = SanctionList
-#     template_name = "sanction/sanction_list.html"
-#     context_object_name = "sanctions"
-#
-#     def get_queryset(self):
-#         sanction_all = SanctionList.objects.all()
-#         page = int(self.request.GET.get('p', 1))
-#         paginator = Paginator(sanction_all, 10)
-#         queryset = paginator.get_page(page)
-#         return queryset
 
 
 def sanction_list(request):
@@ -49,28 +35,30 @@ def sanction_list(request):
 
 
 def result(request):
-    query = request.GET.get('query')
-    if query:
-        qs = SanctionList.objects.filter(name__contains=query)
-        paginator = Paginator(qs, 10)
-        page = request.GET.get('page', 1)
-        sanctions = paginator.get_page(page)
+    # query = request.GET.get('query')
+    # # if query:
+    q = request.GET.get('q', '')
+    qs = SanctionList.objects.filter(name__contains=q)
+    paginator = Paginator(qs, 10)
+    page = request.GET.get('page', 1)
+    sanctions = paginator.get_page(page)
 
-        page_numbers_range = 10
-        max_index = len(paginator.page_range)
-        current_page = int(page) if page else 1
-        start_index = int((current_page - 1) / page_numbers_range) * page_numbers_range
-        end_index = start_index + page_numbers_range
+    page_numbers_range = 10
+    max_index = len(paginator.page_range)
+    current_page = int(page) if page else 1
+    start_index = int((current_page - 1) / page_numbers_range) * page_numbers_range
+    end_index = start_index + page_numbers_range
 
-        if end_index >= max_index:
-            end_index = max_index
-        paginator_range = paginator.page_range[start_index:end_index]
+    if end_index >= max_index:
+        end_index = max_index
+    paginator_range = paginator.page_range[start_index:end_index]
 
-        context = {
-            'sanctions': sanctions,
-            'sanction_list': qs,
-            'paginator_range': paginator_range,
-        }
+    context = {
+        'sanctions': sanctions,
+        'sanction_list': qs,
+        'paginator_range': paginator_range,
+        'q': q,
+    }
     return render(request, 'sanction/result.html', context)
 
 # Create your views here.
