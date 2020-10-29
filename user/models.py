a5a1619e707a7ec86deae257363695152dd379e0
Joonkyo Seo <jkseo50@gmail.com>
2020-10-29 17:34:28 +0900

    login error 수정 (중복 방지 추가요망)

--- user/models.py
+++ user/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
     emp_no = models.CharField(max_length=20, verbose_name='사원번호')
     username = models.CharField(max_length=20, verbose_name='이름')
     email = models.EmailField(max_length=128, verbose_name='이메일주소', default=None)
-    password = models.CharField(max_length=20, verbose_name='비밀번호')
+    password = models.CharField(max_length=100, verbose_name='비밀번호')
     registered_dttm = models.DateTimeField(auto_now_add=True, verbose_name='등록시간')
 
     def __str__(self):
--- user/views.py
+++ user/views.py
@@ -3,15 +3,18 @@ from django.http import HttpResponse
 from django.contrib.auth.hashers import make_password, check_password
 from .models import User
 
+
 def home(request):
     if 'emp_no' in request.session.keys():
         return render(request, 'templates/home.html')
+
 
 def logout(request):
     if request.session.get('user'):
         del(request.session['user'])
 
     return redirect('/')
+
 
 def login(request):
     if request.method == "GET":
@@ -34,19 +37,21 @@ def login(request):
 
     return render(request, 'user/login.html', response_data)
 
+
 def register(request):
     if request.method == "GET":
         return render(request, 'user/register.html')
-    elif request.method == "GET":
-        emp_no = request.POST['emp_no']
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        re_password = request.POST['re_password']
+
+    elif request.method == "POST":
+        emp_no = request.POST.get('emp_no', None)
+        username = request.POST.get('username', None)
+        email = request.POST.get('email', None)
+        password = request.POST.get('password', None)
+        re_password = request.POST.get('re_password', None)
 
         response_data = {}
 
-        if not {emp_no and username and email and password and re_password}:
+        if not (emp_no and username and email and password and re_password):
             response_data['error'] = '모든 값을 입력해야 합니다.'
         elif password != re_password:
             response_data['error'] = '비밀번호가 다릅니다.'
