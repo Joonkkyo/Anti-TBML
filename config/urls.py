9735a062d5a757c96bb1f3eccc498f4f283839ee
youngnrichhh <youngnrichhh@naver.com>
2020-11-13 17:21:49 +0900

    login 수정

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from config import views
-from config.views import HomeView, AboutTV
+from config.views import HomeView, AboutTV, UserCreateView, UserCreateDoneTV
 from django.conf import settings
 from django.conf.urls.static import static
 # from sanction.views import SanctionLV
@@ -14,7 +14,9 @@ urlpatterns = [
     path('document_inspection/', include('document_inspection.urls')),
     path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
-    # path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/register/', UserCreateView.as_view(), name='register'),
+    path('accounts/register/done', UserCreateDoneTV.as_view(), name='register_done'),
     path('about/', AboutTV.as_view(), name='about'),
     # path('sanction/', SanctionLV.as_view(model=SanctionList), name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
